3284318eb5702da395fea1a13809e7b63a17dab3
Alex Zinchenko <me@yumitsu.pro>
2020-02-03 23:55:30 +0300

    One more time

--- jtool2.rb
+++ jtool2.rb
@@ -4,14 +4,13 @@ class Jtool2 < Formula
   url "http://www.newosxbook.com/tools/jtool2.tgz"
   version "2.0"
   sha256 "63850d1efb466a9a1f968ba5c1355fbc5640316f1bee61752ad1dff2fb285b2c"
-  revision 1
+  revision 2
 
   def install
     bin.mkpath
-
-    system "install", "-S", "jtool2", "#{prefix}/bin"
-    system "install", "-S", "jtool2.ELF64", "#{prefix}/bin"
-    system "install", "-S", "disarm", "#{prefix}/bin"
+    rm "jtool2.ELF64"
+    system "install", "-s", "jtool2", "#{prefix}/bin"
+    system "install", "-s", "disarm", "#{prefix}/bin"
     info.install "matchers.txt"
   end
 
