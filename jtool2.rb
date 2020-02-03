3ac1fe9287767e63916e06053cd147342260f475
Alex Zinchenko <me@yumitsu.pro>
2020-02-03 22:53:42 +0300

    Added jtool v2

---
+++ jtool2.rb
@@ -0,0 +1,24 @@
+class Jtool2 < Formula
+  desc "Drop in replacement for otool, nm, strings, and segedit"
+  homepage "http://www.newosxbook.com/tools/jtool.html"
+  url "http://www.newosxbook.com/tools/jtool2.tgz"
+  version "2.0"
+  sha256 "63850d1efb466a9a1f968ba5c1355fbc5640316f1bee61752ad1dff2fb285b2c"
+
+  def install
+    bin.install "jtool2"
+    bin.install "jtool2.ELF64"
+    bin.install "disarm"
+    info.install "matchers.txt"
+  end
+
+  def caveats
+    <<~EOS
+      File with Joker symbols was placed in #{info}
+    EOS
+  end
+
+  test do
+    system "#{bin}/jtool2", "--version"
+  end
+end
