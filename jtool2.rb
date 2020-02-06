afd6ef27bdc63e096a64a606052766ffb9bcc415
Alex Zinchenko <me@yumitsu.pro>
2020-02-07 00:49:35 +0300

    OK, it's not a bug in Brew, it's just me being a little stupid

--- jtool2.rb
+++ jtool2.rb
@@ -1,16 +1,15 @@
 class Jtool2 < Formula
-  desc "Drop in replacement for otool, nm, strings, and segedit"
+B  desc "Drop in replacement for otool, nm, strings, and segedit"
   homepage "http://www.newosxbook.com/tools/jtool.html"
   url "http://www.newosxbook.com/tools/jtool2.tgz"
   version "2.0"
   sha256 "63850d1efb466a9a1f968ba5c1355fbc5640316f1bee61752ad1dff2fb285b2c"
-  revision 3
 
   def install
     bin.mkpath
     rm "jtool2.ELF64"
-    system "install", "-S", "jtool2", "#{prefix}/bin"
-    system "install", "-S", "disarm", "#{prefix}/bin"
+    bin.install "jtool2"
+    bin.install "disarm"
     info.install "matchers.txt"
   end
 
@@ -21,6 +20,6 @@ class Jtool2 < Formula
   end
 
   test do
-    true
+    system "#{bin}/jtool2", "--version"
   end
 end
